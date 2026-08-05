53c0a9245e7e94647a5e1b610a458eef0665bec2
saadferozee <saadferozee@gmail.com>
2026-08-05 17:05:29 +0600

    [inprogress] try to fix network login from mobile, didn't work

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 module.exports = {
-  allowedDevOrigins: ['192.168.0.106'],
+  allowedDevOrigins: ['192.168.0.101'],
 }
 
 const nextConfig: NextConfig = {
